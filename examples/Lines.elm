f255f0f76b0327e487dae3848bfc26b4fdd9d9b3
Tereza Sokol <terezasokol@gmail.com>
2017-12-16 14:58:57 +0100

    Add to example

--- examples/Lines.elm
+++ examples/Lines.elm
@@ -7,6 +7,7 @@ import Lines.Junk as Junk exposing (..)
 import Lines.Color as Color
 import Lines.Dot as Dot
 import Lines.Axis as Axis
+import Lines.Axis.Time as AxisTime
 import Lines.Coordinate as Coordinate
 import Lines.Events as Events
 import Lines.Legends as Legends
@@ -19,11 +20,11 @@ main =
   -- Lines.viewSimple .magnesium .heartattacks [ data1, data2, data3 ]
   Lines.viewCustom
     { frame = Coordinate.Frame (Coordinate.Margin 40 150 90 150) (Coordinate.Size 650 400)
-    , attributes = [ Attributes.style "font-family: monospace;" ] -- Changed from the default!
+    , attributes = [ Attributes.style "font-family: monospace;" ]
     , events = Events.none
     , junk = Junk.none
-    , x = Axis.defaultForDates (Axis.defaultTitle "" 0 0) .date
-    , y = Axis.default (Axis.defaultTitle "" 0 0) .heartattacks
+    , x = AxisTime.default (Axis.defaultTitle "Time" 0 0) .date
+    , y = Axis.default (Axis.defaultTitle "Heart attacks" 0 0) .heartattacks
     , interpolation = Lines.linear
     , legends = Legends.default
     , line = Line.default
@@ -49,35 +50,42 @@ type alias Data =
 
 data1 : List Data
 data1 =
-  [ Data 1 4 2698494243
-  , Data 2 7 (2698494243 + 3633400)
-  , Data 3 6 (2698494243 + 2 * 3633400)
-  , Data 9 3 (2698494243 + 3 * 3633400)
+  [ Data 1 4 (269849424300 + 0 * 7 * 24 * 3600000)
+  , Data 2 7 (269849424300 + 1 * 7 * 24 * 3600000)
+  , Data 3 6 (269849424300 + 2 * 7 * 24 * 3600000)
+  , Data 9 3 (269849424300 + 3 * 7 * 24 * 3600000)
   ]
 
 
 data2 : List Data
 data2 =
-  [ Data 2 2 2698494243
-  , Data 3 4 (2698494243 + 3633400)
-  , Data 4 6 (2698494243 + 2 * 3633400)
-  , Data 5 8 (2698494243 + 3 * 3633400)
+  [ Data 2 2 (269849424300 + 0 * 7 * 24 * 3600000)
+  , Data 3 4 (269849424300 + 1 * 7 * 24 * 3600000)
+  , Data 4 6 (269849424300 + 2 * 7 * 24 * 3600000)
+  , Data 5 8 (269849424300 + 3 * 7 * 24 * 3600000)
   ]
 
 
 data3 : List Data
 data3 =
-  [ Data 2 5 2698494243
-  , Data 3 2 (2698494243 + 3633400)
-  , Data 4 8 (2698494243 + 2 * 3633400)
-  , Data 5 6 (2698494243 + 3 * 3633400)
+  [ Data 2 5 (269849424300 + 0 * 7 * 24 * 3600000)
+  , Data 3 2 (269849424300 + 1 * 7 * 24 * 3600000)
+  , Data 4 8 (269849424300 + 2 * 7 * 24 * 3600000)
+  , Data 5 6 (269849424300 + 3 * 7 * 24 * 3600000)
+  ]
+
+
+data3_5 : List Data
+data3_5 =
+  [ Data 6 6 (269849424300 + 4 * 7 * 24 * 3600000)
+  , Data 7 6 (269849424300 + 5 * 7 * 24 * 3600000)
   ]
 
 
 data4 : List Data
 data4 =
-  [ Data 5 6 (1512495283 + 2 * 3633400)
-  , Data 6 9 (1512495283 + 3 * 3633400)
+  [ Data 5 6 (1512495283 + 2 * 28 * 24 * 3633400)
+  , Data 6 9 (1512495283 + 3 * 28 * 24 * 3633400)
   ]
 
 
