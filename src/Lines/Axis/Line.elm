b7c59050019f58dad2a955c88a4d47b468e57396
Tereza Sokol <terezasokol@gmail.com>
2017-12-29 19:39:46 +0100

    Add line api

---
+++ src/Lines/Axis/Line.elm
@@ -0,0 +1,44 @@
+module Lines.Axis.Line exposing (Line, default, fat, Config, custom)
+
+
+import Svg exposing (Attribute)
+import Lines.Color as Color
+import Internal.Coordinate as Coordinate
+import Internal.Axis.Line as Line
+
+
+{-| -}
+type alias Line msg =
+  Line.Line msg
+
+
+{-| -}
+default : Line msg
+default =
+  Line.default
+
+
+{-| -}
+fat : Line msg
+fat =
+  Line.fat
+
+
+
+-- CUSTOM
+
+
+{-| -}
+type alias Config msg =
+  { color : Color.Color
+  , width : Float
+  , events : List (Attribute msg)
+  , start : Float
+  , end : Float
+  }
+
+
+{-| -}
+custom : (Coordinate.Range -> Config msg) -> Line msg
+custom =
+  Line.custom
