205b3dd0a2fe29ee0576ebb76f4c22681ebfc211
Tereza Sokol <terezasokol@gmail.com>
2017-12-30 15:01:06 +0100

    Update hint example

--- examples/Hint.elm
+++ examples/Hint.elm
@@ -1,15 +1,19 @@
 module HintExample exposing (main)
 
 import Html exposing (Html, div, h1, node, p, text)
-import Lines as Lines exposing (..)
-import Lines.Axis as Axis
-import Lines.Color as Color
-import Lines.Coordinate as Coordinate exposing (..)
-import Lines.Dot as Dot
-import Lines.Events as Events
+import Lines as Lines
 import Lines.Junk as Junk exposing (..)
+import Lines.Color as Color
+import Lines.Dot as Dot
+import Lines.Axis as Axis
+import Lines.Axis.Title as Title
+import Lines.Axis.Range as Range
+import Lines.Axis.Intersection as Intersection
+import Lines.Coordinate as Coordinate
 import Lines.Legends as Legends
 import Lines.Line as Line
+import Lines.Events as Events
+import Lines.Legends as Legends
 import Svg exposing (Attribute, Svg, g, text_, tspan)
 import Svg.Attributes as SvgA
 
@@ -18,7 +22,7 @@ import Svg.Attributes as SvgA
 
 
 type alias Model =
-    { hovering : Maybe Data }
+    { hovering : Maybe Info }
 
 
 initialModel : Model
@@ -31,7 +35,7 @@ initialModel =
 
 
 type Msg
-    = Hover (Maybe Data)
+    = Hover (Maybe Info)
 
 
 update : Msg -> Model -> Model
@@ -48,43 +52,58 @@ update msg model =
 view : Model -> Svg Msg
 view model =
     Lines.viewCustom
-        { frame = Frame (Margin 40 150 90 150) (Size 650 400)
-        , attributes = [ SvgA.style "font-family: monospace;" ]
-        , junk = Maybe.map junk model.hovering |> Maybe.withDefault Junk.none
-        , x = Axis.default (Axis.defaultTitle "Year" 0 0) .year
-        , y = Axis.default (Axis.defaultTitle "Cats" 0 0) .cats
-        , interpolation = Lines.monotone
-        , areaOpacity = 0
-        , id = "hey"
-        , events = Events.default Hover
-        , legends = Legends.default
-        , line = Line.wider 2
-        , dot = Dot.emphasizable (Dot.disconnected 30 2) (Dot.aura 20 5 0.25) (Dot.isMaybe model.hovering)
-        }
-        [ Lines.line Color.blue Dot.circle "Non-binary" data1
-        , Lines.line Color.orange Dot.triangle "Women" data3
-        , Lines.line Color.pink Dot.square "Men" data2
-        ]
+      { margin = Coordinate.Margin 40 150 90 150
+      , attributes = [ SvgA.style "font-family: monospace;" ]
+      , events = Events.default Hover
+      , x =
+          { title = Title.default "age (years)"
+          , variable = .age
+          , pixels = 650
+          , padding = 20
+          , range = Range.default
+          , axis = Axis.time (Axis.around 4)
+          }
+      , y =
+          { title = Title.default "weight (kg)"
+          , variable = .weight
+          , pixels = 400
+          , padding = 20
+          , range = Range.default
+          , axis = Axis.float (Axis.exactly 10)
+          }
+      , intersection = Intersection.default
+      , junk = Maybe.map junk model.hovering |> Maybe.withDefault Junk.none
+      , interpolation = Lines.monotone
+      , legends = Legends.default
+      , line = Line.wider 2
+      , dot = Dot.emphasizable (Dot.disconnected 10 2) (Dot.aura 7 5 0.25) (Dot.isMaybe model.hovering)
+      , areaOpacity = 0
+      , id = "chart"
+      }
+      [ Lines.line Color.blue Dot.circle "bob" bob
+      , Lines.line Color.orange Dot.triangle "alice" alice
+      , Lines.line Color.pink Dot.square "chuck" chuck
+      ]
 
 
-junk : Data -> Junk.Junk Msg
+junk : Info -> Junk.Junk Msg
 junk hint =
     Junk.custom <| \system ->
       let
           viewHint =
-              g [ transform [ move system hint.year hint.cats, offset 20 10 ] ]
+              g [ transform [ move system hint.age hint.weight, offset 20 10 ] ]
                 [ text_ []
-                    [ viewDimension "Year" hint.year
-                    , viewDimension "Cats" hint.cats
+                    [ viewDimension "weight" hint.weight
+                    , viewDimension "age" hint.age
                     ]
                 ]
 
           viewDimension label value =
             tspan
               [ SvgA.x "0", SvgA.dy "1em" ]
-              [ text <| label ++ ": " ++ toString value ]
+              [ Svg.text <| label ++ ": " ++ toString value ]
       in
-      { below = List.map (Junk.gridHorizontal system []) (Axis.defaultInterval system.y)
+      { below = List.map (Junk.gridHorizontal system []) [ 0, 1, 2 ]
       , above = [ viewHint ]
       , html = []
       }
@@ -107,63 +126,43 @@ tooltip system hovered =
 dimension : String -> Float -> Svg msg
 dimension label value =
   Svg.tspan
-    [ Attributes.x "0", Attributes.dy "1em" ]
+    [ SvgA.x "0", SvgA.dy "1em" ]
     [ Svg.text <| label ++ ": " ++ toString value ]
 
 
 -- DATA
 
 
-type alias Data =
-    { year : Float
-    , cats : Float
-    }
+type alias Info =
+  { age : Float
+  , weight : Float
+  , height : Float
+  , income : Float
+  }
 
 
-data1 : List Data
-data1 =
-    [ Data 1 3
-    , Data 2 4
-    , Data 3 4.5
-    , Data 4 5
-    , Data 5 4.3
-    , Data 6 5
-    , Data 7 6.4
-    , Data 8 6.7
-    , Data 9 6.9
-    , Data 10 9
-    ]
+alice : List Info
+alice =
+  [ Info 4 24 0.94 0
+  , Info 25 75 1.73 25000
+  , Info 43 83 1.75 40000
+  ]
 
 
-data2 : List Data
-data2 =
-    [ Data 1 1
-    , Data 2 2
-    , Data 3 4
-    , Data 4 7
-    , Data 5 8
-    , Data 6 8.2
-    , Data 7 7
-    , Data 8 4
-    , Data 9 3
-    , Data 10 6
-    ]
+bob : List Info
+bob =
+  [ Info 4 22 1.01 0
+  , Info 25 75 1.87 28000
+  , Info 43 77 1.87 52000
+  ]
 
 
-data3 : List Data
-data3 =
-    [ Data 1 5
-    , Data 2 5.7
-    , Data 3 5.2
-    , Data 4 6
-    , Data 5 5.8
-    , Data 6 5.2
-    , Data 7 4
-    , Data 8 3.6
-    , Data 9 6
-    , Data 10 7
-    ]
-
+chuck : List Info
+chuck =
+  [ Info 4 21 0.98 0
+  , Info 25 89 1.83 85000
+  , Info 43 95 1.84 120000
+  ]
 
 
 -- Boring stuff
@@ -177,4 +176,4 @@ main =
 viewJust : (a -> Svg msg) -> Maybe a -> Svg msg
 viewJust view maybe =
     Maybe.map view maybe
-        |> Maybe.withDefault (text "")
+        |> Maybe.withDefault (Svg.text "")
