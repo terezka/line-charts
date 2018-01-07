5d110d62fb05909a12154e85bf979a95361a2daa
Tereza Sokol <terezasokol@gmail.com>
2018-01-07 20:26:46 +0100

    Add range documentation

--- src/Lines/Axis/Range.elm
+++ src/Lines/Axis/Range.elm
@@ -5,8 +5,11 @@ module Lines.Axis.Range exposing (Range, default, padded, window, custom)
 # Quick start
 @docs Range, default
 
+# Configurations
+@docs padded, window
+
 # Customizing
-@docs padded, window, custom
+@docs custom
 
 -}
 
@@ -20,25 +23,30 @@ type alias Range =
   Range.Range
 
 
-{-| -}
+{-| Sets the range to the full range of your data plus 20 pixels on both sides.
+-}
 default : Range
 default =
   Range.default
 
 
-{-| -}
+{-| Add a given amount of pixels to the minimum and maxium respectivily.
+-}
 padded : Float -> Float -> Range
 padded =
   Range.padded
 
 
-{-| -}
+{-| Set the minimum and maxium of your range respectivily.
+-}
 window : Float -> Float -> Range
 window =
   Range.window
 
 
-{-| -}
+{-| Given your data's range, produce your desired minimum and maxium
+respectivily.
+-}
 custom : (Coordinate.Range -> ( Float, Float )) -> Range
 custom =
   Range.custom
