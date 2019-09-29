fe236e9a3dbc2b74836710be9e2e4ad5f9eb2276
Frank Schmitt <frank@qwhon.de>
2019-09-29 22:58:04 +0200

    Added dependency color-manipulate for example Events1

--- examples/Events1.elm
+++ examples/Events1.elm
@@ -1,33 +1,32 @@
 module Events1 exposing (main)
 
+import Browser
+import Color
+import Color.Manipulate
 import Html exposing (Html, div, h1, node, p, text)
 import Html.Attributes exposing (class)
-import Svg exposing (Attribute, Svg, g, text_, tspan)
 import LineChart as LineChart
-import LineChart.Junk as Junk exposing (..)
-import LineChart.Dots as Dots
-import LineChart.Container as Container
-import LineChart.Interpolation as Interpolation
-import LineChart.Axis.Intersection as Intersection
+import LineChart.Area as Area
 import LineChart.Axis as Axis
-import LineChart.Legends as Legends
-import LineChart.Line as Line
+import LineChart.Axis.Intersection as Intersection
+import LineChart.Container as Container
+import LineChart.Dots as Dots
 import LineChart.Events as Events
 import LineChart.Grid as Grid
+import LineChart.Interpolation as Interpolation
+import LineChart.Junk as Junk exposing (..)
 import LineChart.Legends as Legends
-import LineChart.Area as Area
-import Color
-import Browser
-
+import LineChart.Line as Line
+import Svg exposing (Attribute, Svg, g, text_, tspan)
 
 
 main : Program () Model Msg
 main =
-  Browser.sandbox
-    { init = init
-    , update = update
-    , view = view
-    }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
 
@@ -48,14 +47,14 @@ init =
 
 
 type Msg
-  = Hover (Maybe Info)
+    = Hover (Maybe Info)
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Hover hovering ->
-      { model | hovering = hovering }
+    case msg of
+        Hover hovering ->
+            { model | hovering = hovering }
 
 
 
@@ -64,88 +63,101 @@ update msg model =
 
 view : Model -> Svg Msg
 view model =
-  Html.div
-    [ class "container" ]
-    [ chart model ]
+    Html.div
+        [ class "container" ]
+        [ chart model ]
 
 
 chart : Model -> Html.Html Msg
 chart model =
-  LineChart.viewCustom
-    { y = Axis.default 450 "Weight" .weight
-    , x = Axis.default 700 "Age" .age
-    , container = Container.styled "line-chart-1" [ ( "font-family", "monospace" ) ]
-    , interpolation = Interpolation.default
-    , intersection = Intersection.default
-    , legends = Legends.default
-    , events = Events.hoverOne Hover
-    , junk = Junk.default
-    , grid = Grid.default
-    , area = Area.default
-    , line =
-        Line.hoverOne model.hovering
+    LineChart.viewCustom
+        { y = Axis.default 450 "Weight" .weight
+        , x = Axis.default 700 "Age" .age
+        , container = Container.styled "line-chart-1" [ ( "font-family", "monospace" ) ]
+        , interpolation = Interpolation.default
+        , intersection = Intersection.default
+        , legends = Legends.default
+        , events = Events.hoverOne Hover
+        , junk = Junk.default
+        , grid = Grid.default
+        , area = Area.default
+        , line =
+            Line.hoverOne model.hovering
+
         -- customLineConfig model.hovering
-    , dots =
-        Dots.hoverOne model.hovering
+        , dots =
+            Dots.hoverOne model.hovering
+
         -- customDotsConfig model.hovering
-    }
-    [ LineChart.line Color.orange Dots.triangle "Chuck" chuck
-    , LineChart.line Color.yellow Dots.circle "Bobby" bobby
-    , LineChart.line Color.purple Dots.diamond "Alice" alice
-    ]
+        }
+        [ LineChart.line Color.orange Dots.triangle "Chuck" chuck
+        , LineChart.line Color.yellow Dots.circle "Bobby" bobby
+        , LineChart.line Color.purple Dots.diamond "Alice" alice
+        ]
 
 
 customLineConfig : Maybe Info -> Line.Config Info
 customLineConfig maybeHovered =
-  let
-    styleDefault =
-      Line.style 1 identity
+    let
+        styleDefault =
+            Line.style 1 identity
 
-    styleHovered =
-      Line.style 2 identity
+        styleHovered =
+            Line.style 2 identity
 
-    styleNotHovered =
-      Line.style 1 (Color.Manipulate.lighten 0.3)
+        styleNotHovered =
+            Line.style 1 (Color.Manipulate.lighten 0.3)
 
-    lineConfig data = -- `data` being all the data for a line
-      case maybeHovered of
-        Just hovered ->
-          if List.any (dotIsHovered maybeHovered) data
-            -- This line is hovered
-            then styleHovered
-            -- Some line is hovered, but not this one
-            else styleNotHovered
+        lineConfig data =
+            -- `data` being all the data for a line
+            case maybeHovered of
+                Just hovered ->
+                    if
+                        List.any (dotIsHovered maybeHovered) data
+                        -- This line is hovered
+                    then
+                        styleHovered
+                        -- Some line is hovered, but not this one
 
-        Nothing ->
-          -- No line is hovered
-          styleDefault
-  in
-  Line.custom lineConfig
+                    else
+                        styleNotHovered
+
+                Nothing ->
+                    -- No line is hovered
+                    styleDefault
+    in
+    Line.custom lineConfig
 
 
 customDotsConfig : Maybe Info -> Dots.Config Info
 customDotsConfig maybeHovered =
-  let
-    styleDefault =
-      Dots.empty 5 2
+    let
+        styleDefault =
+            Dots.empty 5 2
 
-    styleHover =
-      Dots.full 8
+        styleHover =
+            Dots.full 8
 
-    styleLegend data = -- `data` being all the data for a line
-      if List.any (dotIsHovered maybeHovered) data
-        then styleHover
-        else styleDefault
+        styleLegend data =
+            -- `data` being all the data for a line
+            if List.any (dotIsHovered maybeHovered) data then
+                styleHover
 
-    styleIndividual datum = -- `datum` being a single data point on a line
-      if dotIsHovered maybeHovered datum
-        then styleHover
-        else styleDefault
-  in
-  Dots.customAny
-    { legend = styleLegend
-    , individual = styleIndividual
-    }
+            else
+                styleDefault
+
+        styleIndividual datum =
+            -- `datum` being a single data point on a line
+            if dotIsHovered maybeHovered datum then
+                styleHover
+
+            else
+                styleDefault
+    in
+    Dots.customAny
+        { legend = styleLegend
+        , individual = styleIndividual
+        }
 
 
 
@@ -154,42 +166,43 @@ customDotsConfig maybeHovered =
 
 dotIsHovered : Maybe Info -> Info -> Bool
 dotIsHovered maybeHovered datum =
-  Just datum == maybeHovered
+    Just datum == maybeHovered
+
 
 
 -- DATA
 
 
 type alias Info =
-  { age : Float
-  , weight : Float
-  , height : Float
-  , income : Float
-  }
+    { age : Float
+    , weight : Float
+    , height : Float
+    , income : Float
+    }
 
 
 alice : List Info
 alice =
-  [ Info 10 34 1.34 0
-  , Info 16 42 1.62 3000
-  , Info 25 75 1.73 25000
-  , Info 43 83 1.75 40000
-  ]
+    [ Info 10 34 1.34 0
+    , Info 16 42 1.62 3000
+    , Info 25 75 1.73 25000
+    , Info 43 83 1.75 40000
+    ]
 
 
 bobby : List Info
 bobby =
-  [ Info 10 38 1.32 0
-  , Info 17 69 1.75 2000
-  , Info 25 75 1.87 32000
-  , Info 43 77 1.87 52000
-  ]
+    [ Info 10 38 1.32 0
+    , Info 17 69 1.75 2000
+    , Info 25 75 1.87 32000
+    , Info 43 77 1.87 52000
+    ]
 
 
 chuck : List Info
 chuck =
-  [ Info 10 42 1.35 0
-  , Info 15 72 1.72 1800
-  , Info 25 89 1.83 85000
-  , Info 43 95 1.84 120000
-  ]
+    [ Info 10 42 1.35 0
+    , Info 15 72 1.72 1800
+    , Info 25 89 1.83 85000
+    , Info 43 95 1.84 120000
+    ]
